972c1f5c651a407f6629c8d4d9740f638ff44b5f
xdrm-brackets <doowap31@gmail.com>
2015-09-06 15:01:55 +0200

    Creating docs folder

--- code/bmp.py
+++ code/bmp.py
@@ -15,7 +15,7 @@ with open(sys.argv[1]) as f:
 	for byte in f.read():
 		tmp += str( hex( ord(byte) ) )[2:] + ","
 
-headerSize = ( 14 + 9 ) * 8
+headerSize = ( 2 + 4 + 2 + 2 + 4 + 4 + 4 + 4 + 2 + 2 + 4 + 4 + 4 + 4 + 4 + 4 )
 header = tmp[:headerSize]
 content = tmp[headerSize:]	
 
